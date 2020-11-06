987a16359538dceb1af1a390130a803ad298da8c
thisisyiwei <yi.wei@chicagobooth.edu>
2020-11-06 21:56:54 +0000

    User show details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,19 @@ def index
   render ({ :template => "user_templates/index.html.erb"})
 end
 
+def show 
+
+  url_username = params.fetch("path_username")
+  matching_usernames = User.where({ :username => url_username})
+
+  @the_user = matching_usernames.first
+
+  if @the_user == nil
+    redirect_to("/404")
+  else
+    render ({ :template => "user_templates/show.html.erb"})
+  end
+end
 
 
 end
